76f31d5607227f74a0714ddbef918f2a31b0ade4
noragami988 <noragami988@gmail.com>
2019-05-15 20:38:59 +0700

    modify h an c search

--- search.c
+++ search.c
@@ -2,11 +2,10 @@
 
 int NameOfFiles(char argv[])
 {
-    printf(argv);
     FILE *NamesOfFiles;
     NamesOfFiles = fopen("./txt/NamesOfFiles.txt", "w+");
 
-    DIR *mydir = opendir(argv); // вместо / напиши нужный адрес
+    DIR *mydir = opendir(argv);
     if (mydir == NULL)
     {
         perror("opendir");
@@ -15,11 +14,14 @@ int NameOfFiles(char argv[])
     struct dirent *entry;
     while (entry = readdir(mydir))
     {
-
-        printf("%s\n", entry->d_name);
         if ((strcmp(entry->d_name, "NamesOfFiles.txt")) && (strcmp(entry->d_name, ".")) && (strcmp(entry->d_name, "..")))
-        fprintf(NamesOfFiles, "%s\n", entry->d_name);
+            fprintf(NamesOfFiles, "%s\n", entry->d_name);
     }
     closedir(mydir);
     return 0;
+}
+
+void Search(char name[], char w_search[])
+{
+    printf("hello, world");
 }
