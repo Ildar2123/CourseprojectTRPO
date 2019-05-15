af0eca9cdd8b0f8ab16f494c0b7c07d2b414a8f1
noragami988 <noragami988@gmail.com>
2019-05-15 15:20:27 +0700

    Reading Console Arguments

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <dirent.h>
-int main()
+#include <string.h>
+int main(int argc, char **argv)
 {
+    printf("%s\n%s\n%s\n",argv[1],argv[2],argv[3]);
     DIR *mydir = opendir("/"); // вместо / напиши нужный адрес
     if(mydir == NULL) {
         perror("opendir");
