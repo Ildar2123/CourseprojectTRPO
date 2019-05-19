de959a4a976539148bd54133eb9f7db22ebfbff5
Ildar2123 <ildar@DESKTOP-2BPBJTP.localdomain>
2019-05-19 22:56:35 +0700

    test

--- search.h
+++ search.h
@@ -4,10 +4,4 @@
 #include <dirent.h>
 
 int NameOfFiles(char argv[]);
-<<<<<<< HEAD
-void Search(char name[],char w_search[]);
-||||||| merged common ancestors
-void Search(name,w_search);
-=======
 void Search(char where_search[], char what_search[]);
->>>>>>> test_ildar
