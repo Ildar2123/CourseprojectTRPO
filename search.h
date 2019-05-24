b1fd1a104c59662a921988ebdb1e8882b43c51c8
Ildar2123 <ildar@DESKTOP-2BPBJTP.localdomain>
2019-05-24 15:26:06 +0700

    changed searh.h

--- search.h
+++ search.h
@@ -4,10 +4,4 @@
 #include <dirent.h>
 
 int NameOfFiles(char argv[]);
-<<<<<<< HEAD
 void Search(char where_search[], char what_search[]);
-||||||| merged common ancestors
-void Search(char name[],char w_search[]);
-=======
-void Search(char w_search[]);
->>>>>>> 0e8fde6b2dad7c4561a00d8096fe75f87968baed
