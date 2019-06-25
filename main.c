00700a273fa5d4d7db9c477a50960cc75856a79c
Ildar2123 <ildar@DESKTOP-2BPBJTP.localdomain>
2019-06-25 09:40:52 +0700

    format is done

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include "search.h"
 
-int main(int argc, char **argv){
+int main(int argc, char** argv)
+{
     NameOfFiles(argv[2]);
-    Search(argv[2],argv[1]);
+    Search(argv[2], argv[1]);
     return 0;
 }
